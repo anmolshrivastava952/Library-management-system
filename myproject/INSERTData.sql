4b7597d5aba885167eb117322576d65b38d83abb
anmol <anmolshrivastava952@gmail.com>
2023-12-07 14:11:59 +0530

    added sql file which could be run to populate books table

---
+++ myproject/INSERTData.sql
@@ -0,0 +1,12 @@
+INSERT INTO myproject_books (title,description) VALUES ('The Catcher in the Rye', 'A classic novel exploring the struggles of teenage angst and the search for identity.'),
+('To Kill a Mockingbird', 'Harper Lees masterpiece addressing racial injustice in the American South.'),
+('1984', 'George Orwells dystopian vision of a totalitarian future and the power of thought control.'),
+('The Great Gatsby', 'F. Scott Fitzgeralds portrayal of the Roaring Twenties and the elusive American Dream.'),
+('One Hundred Years of Solitude', 'Gabriel García Márquezs magical realist saga spanning seven generations in Macondo.'),
+('The Hobbit', 'J.R.R. Tolkiens enchanting tale of Bilbo Baggins unexpected adventure in Middle-earth.'),
+('Pride and Prejudice', 'Jane Austens timeless romantic novel exploring social class and personal growth.'),
+('The Alchemist', 'Paulo Coelhos philosophical journey of self-discovery and the pursuit of ones dreams.'),
+('Brave New World', 'Aldous Huxleys exploration of a futuristic society controlled by technology and conformity.'),
+('The Harry Potter Series', 'J.K. Rowlings magical saga following the young wizard Harry Potters adventures at Hogwarts.')
+
+Select * from myproject_books
